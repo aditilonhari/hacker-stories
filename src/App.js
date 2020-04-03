47657ecb177d1534a267ec463c71990798eb7765
Aditi <alonhari@newrelic.com>
2020-04-02 18:27:37 -0700

    Adding Common CSS styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
   const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
@@ -90,17 +91,13 @@ const App = () => {
   }, [handleFetchStories]);
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
-      <hr />
       {stories.isError && <p>Something went wrong ...</p>}{" "}
       {stories.isLoading ? (
         <p>Loading ...</p>
@@ -112,7 +109,8 @@ const App = () => {
 };
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
+    {" "}
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -121,7 +119,11 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
     >
       <strong>Search:</strong>
     </InputWithLabel>
-    <button type="submit" disabled={!searchTerm}>
+    <button
+      type="submit"
+      disabled={!searchTerm}
+      className="button button_large"
+    >
       Submit
     </button>
   </form>
@@ -135,9 +137,17 @@ const InputWithLabel = ({
   children
 }) => (
   <>
-    <label htmlFor={id}>{children}</label>
+    <label htmlFor={id} className="label">
+      {children}
+    </label>
     &nbsp;
-    <input id={id} type={type} value={value} onChange={onInputChange} />
+    <input
+      id={id}
+      type={type}
+      value={value}
+      className="input"
+      onChange={onInputChange}
+    />
   </>
 );
 
@@ -146,24 +156,24 @@ const List = ({ list, onRemoveItem }) =>
     <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
   ));
 
-const Item = ({ item, onRemoveItem }) => {
-  const handleRemoveItem = () => onRemoveItem(item);
-
-  return (
-    <div>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" onClick={handleRemoveItem}>
-          Dismiss
-        </button>
-      </span>
-    </div>
-  );
-};
+const Item = ({ item, onRemoveItem }) => (
+  <div className="item">
+    <span style={{ width: "40%" }}>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span style={{ width: "30%" }}>{item.author}</span>
+    <span style={{ width: "10%" }}>{item.num_comments}</span>
+    <span style={{ width: "10%" }}>{item.points}</span>
+    <span style={{ width: "10%" }}>
+      <button
+        type="button"
+        onClick={() => onRemoveItem(item)}
+        className="button button_small"
+      >
+        Dismiss
+      </button>
+    </span>
+  </div>
+);
 
 export default App;
